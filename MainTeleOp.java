e06a1e490fd978694e0abba69ae603aa5a8d2787
MrCoderBoy345 <kaarthik.hari@gmail.com>
2019-10-08 21:39:21 -0400

    added limits on the servo & updated controls

--- MainTeleOp.java
+++ MainTeleOp.java
@@ -15,6 +15,9 @@ public class MainTeleOp extends LinearOpMode {
     private DcMotor right4Bar;
     private Servo clawServo;
 
+    static final double MAX_POS = .545;
+    static  final double MIN_POS = .5127;
+
     /**
      * This function is executed when this Op Mode is selected from the Driver Station.
      */
@@ -25,6 +28,7 @@ public class MainTeleOp extends LinearOpMode {
         left4Bar = hardwareMap.dcMotor.get("left_four_bar");
         right4Bar = hardwareMap.dcMotor.get("right_four_bar");
         clawServo = hardwareMap.servo.get("claw_servo");
+        clawServo.setPosition(.53);
 
         // Reverse one of the drive motors.
         // You will have to determine which motor to reverse for your robot.
@@ -38,18 +42,39 @@ public class MainTeleOp extends LinearOpMode {
             // Put run blocks here.
             while (opModeIsActive()) {
                 // Put loop blocks here.
+
                 // Use left stick to drive and right stick to turn
                 // The Y axis of a joystick ranges from -1 in its topmost position
                 // to +1 in its bottommost position. We negate this value so that
                 // the topmost position corresponds to maximum forward power.
-                leftDrive.setPower(-gamepad1.left_stick_y + gamepad1.left_stick_x);
-                rightDrive.setPower(-gamepad1.left_stick_y - gamepad1.left_stick_x);
+                // leftDrive.setPower(-gamepad1.left_stick_y + gamepad1.left_stick_x);
+                // rightDrive.setPower(-gamepad1.left_stick_y - gamepad1.left_stick_x);
 
-                left4Bar.setPower(left4Bar.getPower() + gamepad1.left_stick_y/8); //attempt to move 4bar up/down with the joystick (NOT WORKING YET)
-                right4Bar.setPower(right4Bar.getPower() + gamepad1.right_stick_y/8);
+                // apparently the team would like a tank drive instead of the POV drive
+                // Tank Mode uses one stick to control each wheel.
+                // - This requires no math, but it is hard to drive forward slowly and keep straight.
+                 leftDrive.setPower(-gamepad1.left_stick_y);
+                 rightDrive.setPower(-gamepad1.right_stick_y);
+
+                if (gamepad1.right_bumper){
+                    left4Bar.setPower(left4Bar.getPower() + .01); //move 4bar up with the right bumper (kinda finicky right now)
+                    right4Bar.setPower(right4Bar.getPower() + .01);
+                }
+                if (gamepad1.left_bumper){
+                    left4Bar.setPower(left4Bar.getPower() - .01); //move 4bar down with the left bumper (kinda finicky right now)
+                    right4Bar.setPower(right4Bar.getPower() - .01);
+                }
 
                 // to do: change the claw so pressing some button will open it and vice verse for closing
-                clawServo.setPosition(clawServo.getPosition() + (gamepad1.right_stick_x *.01));
+
+                //if the servo's new position after being changed is greater than what is allowed, DON'T allow it to change in that direction
+                if (clawServo.getPosition() + (gamepad1.right_trigger *.001) < MAX_POS && gamepad1.left_trigger == 0) {
+                    clawServo.setPosition(clawServo.getPosition() + (gamepad1.right_trigger * .001));
+                }
+                //^ reversed for LT
+                if (clawServo.getPosition() + (gamepad1.left_trigger *-.001) > MIN_POS && gamepad1.right_trigger == 0) {
+                    clawServo.setPosition(clawServo.getPosition() + (gamepad1.left_trigger * -.001));
+                }
 
                 telemetry.addData("Claw Servo Position", clawServo.getPosition());
                 telemetry.addData("Left 4Bar Position", left4Bar.getPower());
